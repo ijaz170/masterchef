7d2cd0e00bdcb3d87cc3767fdb35161bc39f076b
Ijaz ul hassan <161230612+ijaz170@users.noreply.github.com>
2024-11-03 05:47:13 -0800

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBv-xvTRJqhejhRTpoLl6WJYn5W_Kb2syg",
-  authDomain: "ijazassistant-60618.firebaseapp.com",
-  projectId: "ijazassistant-60618",
-  storageBucket: "ijazassistant-60618.firebasestorage.app",
-  messagingSenderId: "381665570479",
-  appId: "1:381665570479:web:8c132497abd372bcd52e5a"
+  
 };
 
 const app = initializeApp(firebaseConfig);
